2b35c80f3ef8245a6a029bb9725affca31c8c61c
walkmanx21 <walkmanx21@gmail.com>
2025-04-12 21:05:14 +0300

    добавление в сервлет всех вариантов ошибок клиента с укаханием соответствующих кодов ответа и сообщениям

--- src/main/java/servlet/OneCurrencyServlet.java
+++ src/main/java/servlet/OneCurrencyServlet.java
@@ -13,27 +13,51 @@ import entity.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet(urlPatterns = {"/currency/USD", "/currency/EUR", "/currency/JPY", "/currency/RUB", "/currency/CHF"})
 @WebServlet("/currency/*")
 
 public class OneCurrencyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String servletPathInfo = req.getPathInfo();
-        String currencyCode = servletPathInfo.substring(1, 4);
-
-        CurrencyDto currencyDto = new CurrencyDto(currencyCode);
-        OneCurrencyService oneCurrencyService = OneCurrencyService.getInstance();
-        Currency currency = oneCurrencyService.createCurrency(currencyDto);
-
-        String currencyJsonString = new Gson().toJson(currency);
-
         PrintWriter out = resp.getWriter();
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        out.print(currencyJsonString);
-        out.flush();
+        String servletPathInfo = req.getPathInfo();
+        boolean noCode = servletPathInfo.length() == 1;
+
+        if (noCode) {
+            resp.setStatus(400);
+            resp.setContentType("text/html");
+            resp.setCharacterEncoding("UTF-8");
+            out.print("Код валюты отсутствует в адресе");
+            return;
+        }
+
+        if (servletPathInfo.length() == 4) {
+            String currencyCode = servletPathInfo.substring(1, 4).toUpperCase();
+            CurrencyDto currencyDto = new CurrencyDto(currencyCode);
+            OneCurrencyService oneCurrencyService = OneCurrencyService.getInstance();
+            Currency currency = oneCurrencyService.createCurrency(currencyDto);
+
+            if (currency != null) {
+                resp.setStatus(200);
+
+                String currencyJsonString = new Gson().toJson(currency);
+                resp.setContentType("application/json");
+                resp.setCharacterEncoding("UTF-8");
+                out.print(currencyJsonString);
+                out.flush();
+            } else {
+                resp.setStatus(404);
+                resp.setContentType("text/html");
+                resp.setCharacterEncoding("UTF-8");
+                out.print("Валюта не найдена");
+                out.flush();
+            }
+        } else {
+            resp.setStatus(400);
+            resp.setContentType("text/html");
+            resp.setCharacterEncoding("UTF-8");
+            out.print("Некорректный код валюты");
+        }
 
     }
 }
