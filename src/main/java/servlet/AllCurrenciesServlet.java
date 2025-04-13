cee0574ef10df4736b048e4739f9c7a262736564
walkmanx21 <walkmanx21@gmail.com>
2025-04-13 09:31:12 +0300

    создание сервлет AllCurrenciesServlet. Он работает

--- src/main/java/servlet/AllCurrenciesServlet.java
+++ src/main/java/servlet/AllCurrenciesServlet.java
@@ -1,5 +1,8 @@
 package servlet;
 
+import com.google.gson.Gson;
+import dao.CurrencyDao;
+import entity.Currency;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,11 +10,28 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("/currencies")
 public class AllCurrenciesServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        CurrencyDao currencyDao = CurrencyDao.getInstance();
+        List<Currency> currencies = currencyDao.findAllEntities();
+
+        String currenciesJsonString = new Gson().toJson(currencies);
+        print(resp, 200, "application/json", currenciesJsonString);
+    }
+
+    private void print (HttpServletResponse resp, int status, String contentType, String message) throws IOException {
+        PrintWriter out = resp.getWriter();
+        resp.setStatus(status);
+        resp.setContentType(contentType);
+        resp.setCharacterEncoding("UTF-8");
+        out.print(message);
+        out.flush();
     }
 }
---
+++ src/main/java/util/PrintWriter.java
@@ -0,0 +1,4 @@
+package util;
+
+public final class PrintWriter {
+}
