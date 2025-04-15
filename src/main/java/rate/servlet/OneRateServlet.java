2aacf9d7e130f9a4b5a59709ddeea1706c7759c9
walkmanx21 <walkmanx21@gmail.com>
2025-04-15 14:57:59 +0300

    промежуточное сохранение - в OneRateServlet прописан отдельный class и метод для подтягивания параметров запроса

--- src/main/java/rate/servlet/OneRateServlet.java
+++ src/main/java/rate/servlet/OneRateServlet.java
@@ -2,6 +2,9 @@ package rate.servlet;
 
 import com.google.gson.Gson;
 import exception.ExchangeRateNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import rate.dto.RateRequestDto;
 import rate.dto.RateResponseDto;
 import jakarta.servlet.ServletException;
@@ -13,6 +16,7 @@ import rate.RateService;
 import util.ResponsePrintWriter;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 
 @WebServlet("/exchangeRate/*")
@@ -21,18 +25,30 @@ public class OneRateServlet extends HttpServlet {
     private RateService rateService = RateService.getInstance();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String servletPathInfo = req.getPathInfo();
-        boolean noCurrenciesCode = servletPathInfo.length() == 1;
-
-        if (noCurrenciesCode) {
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PathInfo pathInfo = getPathInfo(req);
+        if (pathInfo == null) {
             ResponsePrintWriter.printResponse(resp, 400, "Коды валют пары отсутствуют в адресе");
             return;
         }
+        String rateString = req.getParameter("rate");
+        if (rateString.contains(",")) {
+            rateString = rateString.replace(',', '.');
+        }
+        BigDecimal rate =  new BigDecimal(rateString);
+        RateRequestDto rateRequestDto = new RateRequestDto(pathInfo.getBaseCurrencyCode(), pathInfo.getTargetCurrencyCode(), rate);
 
-        String baseCurrencyCode = servletPathInfo.substring(1, 4).toUpperCase();
-        String targetCurrencyCode = servletPathInfo.substring(4, 7).toUpperCase();
-        RateRequestDto rateRequestDto = new RateRequestDto(baseCurrencyCode, targetCurrencyCode);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PathInfo pathInfo = getPathInfo(req);
+        if (pathInfo == null) {
+            ResponsePrintWriter.printResponse(resp, 400, "Коды валют пары отсутствуют в адресе");
+            return;
+        }
+        RateRequestDto rateRequestDto = new RateRequestDto(pathInfo.getBaseCurrencyCode(), pathInfo.getTargetCurrencyCode());
         RateResponseDto rateResponseDto = null;
 
         try {
@@ -45,4 +61,25 @@ public class OneRateServlet extends HttpServlet {
         ResponsePrintWriter.printResponse(resp, 200, currencyJsonString);
 
     }
+
+    @Getter
+    @AllArgsConstructor
+    static
+    class PathInfo {
+        String baseCurrencyCode;
+        String targetCurrencyCode;
+    }
+
+    private PathInfo getPathInfo (HttpServletRequest req) {
+        String servletPathInfo = req.getPathInfo();
+        boolean noCurrenciesCode = servletPathInfo.length() == 1;
+        if (noCurrenciesCode) {
+            return null;
+        }
+        String baseCurrencyCode = servletPathInfo.substring(1, 4).toUpperCase();
+        String targetCurrencyCode = servletPathInfo.substring(4, 7).toUpperCase();
+        return new PathInfo(baseCurrencyCode, targetCurrencyCode);
+
+    }
+
 }
